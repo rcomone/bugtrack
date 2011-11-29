c8fe5093fd2e2ba139df1dbf4572307c84a370b7
NiniGeek <ninigeek@gmail.com>
2011-11-29 14:50:08 +0100

    comments

--- application/Core/Bootstrap.php
+++ application/Core/Bootstrap.php
@@ -1,6 +1,15 @@
 <?php 
-
-
+/**
+ * Core_Bootstrap
+ * @category       Bugtrack
+ * @package        Core
+ * @desc           IndexController
+ * @author         Dev1 Lyon <devlyon1@cleo-consulting.fr>
+ * @copyright      DEV LYON
+ * @license        http://framework.zend.com/new-bsd   New BSD License
+ * @version        Release : 1.0 (2011-11-29)
+ *
+ */
 class Core_Bootstrap extends Zend_Application_Module_Bootstrap
 {
     
