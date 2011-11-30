a726432da8b3aca6ea274e5b15626ce46ba2d38c
Jean-Baptiste MONIN <jb.monin@cleo-consulting.fr>
2011-11-30 17:04:52 +0100

    adding tests for user model

---
+++ tests/application/User/models/StaffmembreTest.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+class User_Model_StaffmemberTest
+{
+    public function testGettersAndSettersAreWorkingFine()
+    {
+        $user = new User_Model_Staffmembre();
+        $user->setId(1)
+                ->setFirstname('testfn')
+                ->setLastname('testln')
+                ->setLogin('testl')
+                ->setEmail('teste')
+                ->setPassword('testp');
+       $this->assertEquals(1, $user->getId());    
+       $this->assertEquals('testfn', $user->getFirstname());
+       $this->assertEquals('testln', $user->getLastname());
+       $this->assertEquals('testl', $user->getLogin());
+       $this->assertEquals('teste', $user->getEmail());
+       $this->assertEquals('testp', $user->getPassword());   
+    }
+}
