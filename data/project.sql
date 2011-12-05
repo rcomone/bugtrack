655cb8d0a8efef769eebc52832cd0f223ff3a83a
woodgoal <woodgoal2000@yahoo.fr>
2011-12-05 14:51:58 +0100

    add new project.sql

---
+++ data/project.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (i486)
+--
+-- Host: localhost    Database: bugtrack
+-- ------------------------------------------------------
+-- Server version	5.1.49-3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `project`
+--
+
+DROP TABLE IF EXISTS `project`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `project` (
+  `proj_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `proj_name` varchar(45) NOT NULL,
+  `proj_desc` text NOT NULL,
+  `proj_date` int(10) NOT NULL,
+  `proj_statut` varchar(45) NOT NULL,
+  `proj_hpurl` varchar(155) NOT NULL,
+  `proj_docurl` varchar(155) NOT NULL,
+  `usm_id` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`proj_id`),
+  KEY `fk_usm_id` (`usm_id`),
+  CONSTRAINT `fk_usm_id` FOREIGN KEY (`usm_id`) REFERENCES `user_staffmembre` (`usm_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `project`
+--
+
+LOCK TABLES `project` WRITE;
+/*!40000 ALTER TABLE `project` DISABLE KEYS */;
+/*!40000 ALTER TABLE `project` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-12-05 14:51:00
