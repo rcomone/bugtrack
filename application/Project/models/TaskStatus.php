46152156b8c2d8f9b085a3f58842e12af32b72bc
NiniGeek <ninigeek@gmail.com>
2011-12-06 16:16:37 +0100

    add fluent to models

--- application/Project/models/TaskStatus.php
+++ application/Project/models/TaskStatus.php
@@ -36,43 +36,45 @@
  */
  class Project_Model_TaskStatus
  {
- 	/**
-	 * TaskStatus's status (formerly DB primary key)
-	 * @var int
-	 */
-	private $_id;
-	
-	/**
-	 * TaskStatus's title
-	 * @var string
-	 */
-	private $_title;
-	
-	/**
-	 * @return the $_id
-	 */
-	public function getId() {
-		return $this->_id;
-	}
+     /**
+     * TaskStatus's status (formerly DB primary key)
+     * @var int
+     */
+    private $_id;
+    
+    /**
+     * TaskStatus's title
+     * @var string
+     */
+    private $_title;
+    
+    /**
+     * @return the $_id
+     */
+    public function getId() {
+        return $this->_id;
+    }
 
-	/**
-	 * @param int $_id
-	 */
-	public function setId($_id) {
-		$this->_id = $_id;
-	}
+    /**
+     * @param int $_id
+     */
+    public function setId($_id) {
+        $this->_id = $_id;
+        return $this;
+    }
 
-	/**
-	 * @return the $_title
-	 */
-	public function getTitle() {
-		return $this->_title;
-	}
+    /**
+     * @return the $_title
+     */
+    public function getTitle() {
+        return $this->_title;
+    }
 
-	/**
-	 * @param string $_title
-	 */
-	public function setTitle($_title) {
-		$this->_title = $_title;
-	}
+    /**
+     * @param string $_title
+     */
+    public function setTitle($_title) {
+        $this->_title = $_title;
+        return $this;
+    }
  }
