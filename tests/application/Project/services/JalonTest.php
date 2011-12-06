14b0923f78302f7f4c027a2f4c51afc43a3be90f
Nikoquattro <nikoquattro@gmail.com>
2011-12-06 16:41:26 +0100

    add test Jalon

--- tests/application/Project/services/JalonTest.php
+++ tests/application/Project/services/JalonTest.php
@@ -2,5 +2,27 @@
 
 class Project_Service_JalonTest extends PHPUnit_Framework_TestCase
 {
-
+	public function testGetlistMethodShouldReturnArrayOfJalonObjects()
+    {
+        $jalonService = new Project_Service_Jalon();
+        $result = $jalonService->getList();
+        $this->assertInternalType('array', $result);
+        if (is_array($result)) {
+            $isJalonObject = true;
+            foreach ($result as $jalon) {
+                if (!($jalon instanceof Project_Model_Jalon)) {
+                        $isJalonObject = false;
+                }
+            }
+            $this->assertTrue($isJalonObject);
+        }
+    }
+    
+	public function testDeleteMethodShouldReturnTrue()
+    {
+    	$jalon = new Project_Model_Jalon();
+    	$jalonService = new Project_Service_Jalon();
+        $result = $userService->delete($jalon);
+        $this->assertTrue($result);
+    }
 }
