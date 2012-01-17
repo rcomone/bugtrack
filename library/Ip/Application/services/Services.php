f25bc14b5941065bcff631938455c60b3872b9e3
NiniGeek <ninigeek@gmail.com>
2012-01-17 16:38:25 +0100

    généralisation des services, pourqu'un services aient plusieurs mapper et qu'un service réutilise toujours le bon et lemême mapper

--- library/Ip/Application/services/Services.php
+++ library/Ip/Application/services/Services.php
@@ -1,15 +1,17 @@
 <?php
 abstract class Ip_Application_Service_Services {
-    private $_mapper;
+    private $_mapper = array();
     /**
      * @return the $_mapper
      */
-    public function getMapper ()
+    public function getMapper ($classMapper = null)
     {
         if(is_null($this->_mapper)) {
-            $classMapper = str_replace('Service', 'Model_Mapper', get_class($this));
+            if (is_null($classMapper)){
+                 $classMapper = str_replace('Service', 'Model_Mapper', get_class($this));
+            }
             $this->_mapper = new $classMapper;
         }
-        return $this->_mapper;
+        return $this->_mapper[$classMapper];
     }
 }
