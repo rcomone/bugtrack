61ffa972e742e4a42d322472dabaaa6e45d91066
NiniGeek <ninigeek@gmail.com>
2012-01-18 09:10:09 +0100

    Correction de bug

--- library/Ip/Application/services/Services.php
+++ library/Ip/Application/services/Services.php
@@ -1,16 +1,16 @@
 <?php
-abstract class Ip_Application_Service_Services {
+abstract class Ip_Application_services_Services {
     private $_mapper = array();
     /**
      * @return the $_mapper
      */
     public function getMapper ($classMapper = null)
     {
-        if(is_null($this->_mapper)) {
-            if (is_null($classMapper)){
-                 $classMapper = str_replace('Service', 'Model_Mapper', get_class($this));
-            }
-            $this->_mapper = new $classMapper;
+        if (is_null($classMapper)){
+            $classMapper = str_replace('Service', 'Model_Mapper', get_class($this));
+        }
+        if(empty($this->_mapper[$classMapper])) {
+            $this->_mapper[$classMapper] = new $classMapper;
         }
         return $this->_mapper[$classMapper];
     }
